3e1557135be79e3041faeb30dcd170f08a6440c9
Ricardo Gulias <gulias@DAREDE-001430.darede.corp>
2026-05-13 08:20:45 -0300

    feat: add Dawid as admin for Poland chapter

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 var GJ_CONFIG = {
-  ADMIN_EMAIL: 'ricardo.gulias@darede.com.br',
+  ADMIN_EMAILS: ['ricardo.gulias@darede.com.br', 'drabekdawid@gmail.com'],
   ADMIN_API: 'https://kqiq2bltjd.execute-api.us-east-1.amazonaws.com/admin'
 };
