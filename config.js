acfe6d726972c2cac375acadb9625e050d7536eb
Ricardo Gulias <gulias@DAREDE-001430.darede.corp>
2026-05-19 05:55:52 -0300

    fix: handle MFA_SETUP challenge during login + add Erick/Wagner as admins

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 var GJ_CONFIG = {
-  ADMIN_EMAILS: ['ricardo.gulias@goldenjacketsbrazil.com', 'drabekdawid@gmail.com'],
+  ADMIN_EMAILS: ['ricardo.gulias@goldenjacketsbrazil.com', 'drabekdawid@gmail.com', 'erickmancz@gmail.com', 'wagnermazevedo@hotmail.com'],
   ADMIN_API: 'https://kqiq2bltjd.execute-api.us-east-1.amazonaws.com/admin'
 };
